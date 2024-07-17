02b99315262d9d995b36dd29ae8a38ecc1f295ac
hubchev <DrStephanHuber@yahoo.com>
2024-07-17 16:24:00 +0200

    test2

--- ss_24/desc_ttest/ttest.R
+++ ss_24/desc_ttest/ttest.R
@@ -6,99 +6,139 @@ rm(list = ls())
 
 load("~/Dropbox/hsf/github/ewa/ss_24/read_in_71/data_71.RData")
 
+# Zuerst wandle ich die Daten etwas um, so dass ich keine Faktorvariable mehr habe:
+df_test <- df_cleaned |> 
+  mutate_at(vars(starts_with("item_")), as.double)
+
+
 # ##########################
-# One Sample t-test
+# Ein-Stichproben t-Test
 # ##########################
 
-t.test(df$item_1, mu = 1)
-t.test(df$item_1, mu = 3)
+t.test(df_test$item_1, mu = 1)
+t.test(df_test$item_1, mu = 3)
 
-# by default t.test runs a two sided test:
-t.test(df$item_1, mu = 3, alternative = "two.sided")
+# Standardmäßig führt t.test einen zweiseitigen Test durch:
+t.test(df_test$item_1, mu = 3, alternative = "two.sided")
 
-# you can test 
-t.test(df$item_1, 
+# Sie können einen Test für einen Teil der Daten durchführen, indem Sie ein Argument der t.test-Funktion verwenden:
+t.test(df_test$item_1, 
        mu = 3, 
        alternative = "two.sided",
-       subset(df, group == 0))
+       subset(df_test, group == 0))
 
-t.test(df$item_1, 
+t.test(df_test$item_1, 
        mu = 3,
        alternative = "greater")
 
-t.test(df$item_1, 
+t.test(df_test$item_1, 
        mu = 3,
        alternative = "less")
 
 # ##########################
-# Two sided t-test in R
+# Zweiseitiger t-Test in R
 # ##########################
 
-# Welch test
-t.test(df$item_1, df$item_2, data = df)
+# ----------------------------
+# Für Daten im Breitformat:
+# ----------------------------
 
-# Student's t-test
-t.test(df$item_1, df$item_2, data = df, var.equal = TRUE)
+# Welch-Test
+t.test(df_test$item_1, df_test$item_2, data = df_test)
 
-# Welch test
-t.test(df$item_1, df$item_2, data = df, 
+# Student's t-Test
+t.test(df_test$item_1, df_test$item_2, data = df_test, var.equal = TRUE)
+
+# Welch-Test
+t.test(df_test$item_1, df_test$item_2, data = df_test, 
        paired = TRUE, 
        var.equal = TRUE)
 
 ## Die Antworten kommen von der selben Person, daher paired = TRUE
 
+# ----------------------------
+# Für Daten im Langformat:
+# ----------------------------
+
+# Test, ob das item_1 in beiden Gruppen gleich ist
+
+# Erstelle ein Boxplot von item_1 über Gruppen mit ggplot2
+ggplot(df_test, aes(x = factor(group), y = item_1)) +
+  geom_boxplot() +
+  labs(x = "Group", y = "Item 1") +
+  ggtitle("Boxplot of Item 1 Across Groups")
+
+## Wenn Normalitäts- und Varianzannahmen erfüllt sind:
+t.test(item_1 ~ group, data = df_test, var.equal = FALSE)
+
+
+# OK, aber wie testet man nun,
+# ob die Antworten aller items in den jeweiligen Gruppen gleich sind? 
+
+# Zuerst empfiehlt sich eine Umwandlung in das Langformat:
+df_test_long <- df_test |> 
+  pivot_longer(cols = starts_with("item_"), # Zu pivotierende Spalten
+               names_to = "item",           # Neue Spalte für die Namen der Items
+               values_to = "value")  |>     # Neue Spalte für die Werte der Items
+  select(id, group, complete, item, value)
+
+# Erstelle ein Boxplot von value über Gruppen mit ggplot2
+ggplot(df_test_long, aes(x = factor(group), y = value)) +
+  geom_boxplot() +
+  labs(x = "Group", y = "Item 1") +
+  ggtitle("Boxplot of all items Across Groups")
+
+## Wenn Normalitäts- und Varianzannahmen erfüllt sind:
+
+df_sub_complete <- df_test_long |> 
+  filter(complete == TRUE)
+
+
+t.test(value ~ group, data = df_sub_complete, var.equal = FALSE)
 
 # ##########################
-# check normality assumption
+# Prüfung der Normalitätsannahme
 # ##########################
 
-## With shapiro
-df |> 
+## Mit Shapiro
+df_test |> 
   group_by(group) |> 
   shapiro_test(item_1)
 
-## with qq plots
-ggqqplot(df, x = "item_1", facet.by = "group")
+## Mit QQ-Plots
+ggqqplot(df_test, x = "item_1", facet.by = "group")
 
-# if the p-value is small, we must use a test that does not assume normality 
-# --> Mann-Whitney-U-Test (aka Wilcoxon-Rangsummentest) vergleicht die Mediane 
+# Wenn der p-Wert klein ist, müssen wir einen Test verwenden, der keine Normalverteilung annimmt 
+# --> Mann-Whitney-U-Test (auch Wilcoxon-Rangsummentest genannt) vergleicht die Mediane 
 # von zwei unabhängigen Gruppen und ist robust gegenüber 
 # Nicht-Normalverteilung und unterschiedlichen Varianzen.
 
-
 # ##########################
-# check homogeneity of variances
+# Überprüfung der Varianzhomogenität
 # ##########################
 
-## with levene
-leveneTest(item_1 ~ as.character(group), data = df)
+## Mit Levene
+leveneTest(item_1 ~ as.character(group), data = df_test)
 
-## with bartlett.test
-bartlett.test(item_1 ~ group, data = df)
+## Mit bartlett.test
+bartlett.test(item_1 ~ group, data = df_test)
 
-## with Fligner-Kelleen
-fligner.test(item_1 ~ group, data = df)
+## Mit Fligner-Kelleen
+fligner.test(item_1 ~ group, data = df_test)
 
-# if the p-value is small the variance is different between the groups and 
-# we must use a test that does not assume normality 
-# --> Welch t-test (or Mann-Whitney)
+# Wenn der p-Wert klein ist, ist die Varianz zwischen den Gruppen unterschiedlich und 
+# wir müssen einen Test verwenden, der keine Normalverteilung annimmt 
+# --> Welch-t-Test (oder Mann-Whitney)
 
 # ##########################
 # Test 
 # ##########################
 
-## if normality and variance assumptions are given:
-t.test(item_1 ~ group, data = df, var.equal = FALSE)
+## Wenn Normalitäts- und Varianzannahmen erfüllt sind:
+t.test(item_1 ~ group, data = df_test, var.equal = TRUE)
 
-## if normality is given and normality not:
-t.test(item_1 ~ group, data = df, var.equal = FALSE)
+## Wenn Normalität erfüllt ist und Varianz nicht:
+t.test(item_1 ~ group, data = df_test, var.equal = FALSE)
 
-## if normality is not given:
-wilcox.test(item_1 ~ group, data = df)
-
-
-
-df |> 
-  select(id, group, item_1, item_2) |> 
-  head()
-
+## Wenn Normalität nicht erfüllt ist:
+wilcox.test(item_1 ~ group, data = df_test)
